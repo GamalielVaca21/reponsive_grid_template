886452415805ee32d42032e60c22cfb47470851e
GamalielVaca21 <gamalielvacasolis.com>
2023-09-14 22:45:35 -0600

    Se agrega el proyecto

---
+++ registrate.php
@@ -0,0 +1,22 @@
+<?php require 'controlador.php'; ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/hash.css">
+    <title>Registro</title>
+</head>
+<body>
+    <form id="form1" action="" method="POST" autocomplete="off">
+        <h1>Registro de Usuarios</h1>
+
+        <label for="">Usuario</label>
+        <input type="text" name="usuario_usu" id="">
+
+        <label for="">Password</label>
+        <input type="password" name="password_usu" id="">
+
+        <button type="submit" name="registro">Registro</button>
+    </form>
+</body>
+</html>
